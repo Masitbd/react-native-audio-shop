2a19465249f2ee159567c3d2ebc6cd716466fadf
sarker24 <sarker24@gmail.com>
2023-12-09 14:44:56 +0100

    Add the stack navigation

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,  View } from 'react-native';
+import { colors } from './src/theme/colors';
+import {spacing} from './src/theme/spacing'
+import {useFonts} from 'expo-font'
+import { typography } from './src/theme/typograpy';
+import Text from './src/components/text/text';
+import { NavigationContainer, DarkTheme } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Home from './src/screens/home';
 
 export default function App() {
+  const [loaded] = useFonts({
+    "Antonio-Medium": require("./assets/fonts/Antonio-Medium.ttf"),
+    "Spartan-Bold": require("./assets/fonts/Spartan-Bold.ttf"),
+    "Spartan-Regular": require("./assets/fonts/Spartan-Regular.ttf"),
+  });
+
+  if (!loaded) {
+    return <Text>Font is loading</Text>;
+  }
+
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+     <>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}} >
+         <Stack.Screen name="Home" component={Home} />
+          {/* <Stack.Screen name="Details" component={Details} />  */}
+        </Stack.Navigator>
+      </NavigationContainer>
+      <StatusBar style='light' />
+     </>
+  
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   // backgroundColor: colors.black,
+    
   },
 });
